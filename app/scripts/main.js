cb9d977cdfe78609e7513f471ab590920dbec940
InabaJun <inabajun@InabaJun-no-Mac-mini.local>
2017-11-12 16:35:34 +0900

    kick script by all button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,7 @@
-var element = document.querySelector('button[data-reaction-label="-1"]');
-element.parentNode.removeChild(element);
+var reactionButtons = document.querySelectorAll('button[aria-label="Add your reaction"]');
+reactionButtons.forEach(function(button) {
+	button.addEventListener("click", function(){
+		var element = document.querySelector('button[data-reaction-label="-1"]');
+		element.parentNode.removeChild(element);
+		});
+});
